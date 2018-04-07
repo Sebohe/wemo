6c4d6dc2cd4cef86b931399d7cb15632f193ebf4
Sebastian Bolanos <sebasheston@gmail.com>
2018-04-07 00:46:02 -0700

    added capability to detect

--- cli.py
+++ cli.py
@@ -2,14 +2,27 @@
 import requests
 from wemo_devices import WemoDevice
 import sys
+import os
+from time import sleep
+
+def detectCellPhone():
+    
+    host = '192.168.1.149'
+
+    while True:
+        response = os.system('ping -c 1 ' + host)
+        print (response)
+        sleep(1)
+        if response == False:
+            break
+   
+    return True  
+    
 
 if __name__ == "__main__":
-    
-    print (sys.argv)
+   
     assert(len(sys.argv) <= 3)
-
     myWemo = WemoDevice(sys.argv[1])
-
     if sys.argv[-1] == "on":
         myWemo.on()
         exit()
@@ -17,5 +30,10 @@ if __name__ == "__main__":
     if sys.argv[-1] == "off":
         myWemo.off()
         exit()
+
+    if sys.argv[-1] == "detect":
+        detectCellPhone()
+        myWemo.on()
+        exit() 
      
     myWemo.toggle()
